de94e86012e2c54df52aaa75099439ebae36cffb
Michael Chukwuka <109005549+Jagolabi@users.noreply.github.com>
2023-02-28 07:28:13 -0600

    Update MonteCarloInt.py

--- MonteCarloInt.py
+++ MonteCarloInt.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Feb 24 3:27:14 2023
-@author: johnpaulmbagwu
+Created on Fri Feb 25 11:35:07 2023
+@author: Michael Chukwuka
 """
 
 import numpy as np
@@ -12,7 +12,7 @@ import scipy.integrate as integrate
 
 ###########  MONTE CARLO ####################
 def MonteCarlo(f, a, b, n):
-    u = np.random.uniform(0, 1, n)  # generate random points uniformly in [0,1]
+    u = np.random.uniform(0, 2, n)  # generate random points uniformly in [0,2]
     arr = f(a+(b-a)*u)
     total = ((b-a)/n)*(arr.sum())
     return total
@@ -43,17 +43,17 @@ def trap_rule(f, a, b, n):
 def f(x):
     return (np.sin(x)) ** 10 * np.exp(-x / 5.0)
 ##############################################
-print("We are integrating the function sin(x)^5**10*exp(-x/5) in the interval [20,40]:\n")
+print("We are integrating the function sin(x)^5**10*exp(-x/5) in the interval [20,50]:\n")
 print("Enter the order of the Quadrature methods, or numerical integration to evaluate: ")
 num = int(input())
 I_exacta = 0.038964609200183953761884221510468
-I_MC = MonteCarlo(f, 20.0, 40.0, num)
+I_MC = MonteCarlo(f, 20.0, 50.0, num)
 
 # Evaluate the integral using the trapezoidal rule
-I_trap = trap_rule(f, 20.0, 40.0, num)
+I_trap = trap_rule(f, 20.0, 50.0, num)
 
 # Evaluate the integral using Gaussian quadrature
-I_gauss = integrate.fixed_quad(f, 20.0, 40.0, n=num)
+I_gauss = integrate.fixed_quad(f, 20.0, 50.0, n=num)
 
 print("\nExacta:\n", I_exacta)
 print("Monte Carlo:\n", I_MC)
